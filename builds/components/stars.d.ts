9644162bcfc5bd8f33ebac4afcab017213eb2309
Gabriel Castor <gabriel.castor@normative.io>
2021-03-13 16:18:12 +0100

    add builds

--- builds/components/stars.d.ts
+++
@@ -1,6 +0,0 @@
-import * as BABYLON from "@babylonjs/core/Legacy/legacy";
-export declare function createStars(scene: any): {
-    SPS: BABYLON.SolidParticleSystem;
-    stars: BABYLON.Mesh;
-    starSphere: BABYLON.Mesh;
-};
