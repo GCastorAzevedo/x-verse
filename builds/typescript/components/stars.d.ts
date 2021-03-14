3d1c246ae4ea2808b4e38b28c1385f9d69b20314
Gabriel Castor <gabriel.castor@normative.io>
2021-03-14 14:53:41 +0100

    new build

--- builds/typescript/components/stars.d.ts
+++ builds/typescript/components/stars.d.ts
@@ -1,10 +1,2 @@
 import { Scene } from '@babylonjs/core/scene';
-import { Mesh } from "@babylonjs/core/Meshes/mesh";
-import * as BABYLON from "@babylonjs/core/Legacy/legacy";
-interface IConstellationOutput {
-    SPS: BABYLON.SolidParticleSystem;
-    systemMesh: Mesh;
-    baseModels: Mesh[];
-}
-export declare function createStars(scene: Scene): IConstellationOutput;
-export {};
+export declare function createStars(scene: Scene): Scene;
